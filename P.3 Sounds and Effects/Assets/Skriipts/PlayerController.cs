608d52459ed04a6dc14e3f3225e5e3392b615117
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-10-19 14:26:10 -0700

    Left Off at 3.4.3

--- P.3 Sounds and Effects/Assets/Skriipts/PlayerController.cs
+++ P.3 Sounds and Effects/Assets/Skriipts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,24 +9,27 @@ public class PlayerController : MonoBehaviour
     public float JumpForce = 10f;
         public float GravityModifier = 1f;
     public bool IsOnGround = true;
+    public ParticleSystem explosionParticle;
 
     private Rigidbody _playerRB;
+    private Animator _playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
      _playerRB = GetComponent<Rigidbody>();
-        //_playerRB.AddForce(Vector3.up * 100);
+        _playerAnim = GetComponent<Animator>();
         Physics.gravity *= GravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround && !gameOver)
             {
               _playerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
                IsOnGround = false;
+            _playerAnim.SetTrigger("Jump_trig");
             }
     }
     private void OnCollisionEnter(Collision collision)
@@ -34,10 +38,14 @@ public class PlayerController : MonoBehaviour
         {
             IsOnGround = true;
         }
+
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            explosionParticle.Play();
             gameOver = true;
             Debug.Log("Game Over!");
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int)", 1);
         }
     }
 }
