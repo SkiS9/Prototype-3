ba1554bb605bc9f64ccac9e51cbd2a90579d0942
SkiS9 <146136100+SkiS9@users.noreply.github.com>
2023-10-20 13:45:24 -0700

    Finished Product and "Fun" to Play!

--- P.3 Sounds and Effects/Assets/Skriipts/PlayerController.cs
+++ P.3 Sounds and Effects/Assets/Skriipts/PlayerController.cs
@@ -10,9 +10,13 @@ public class PlayerController : MonoBehaviour
         public float GravityModifier = 1f;
     public bool IsOnGround = true;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     private Rigidbody _playerRB;
     private Animator _playerAnim;
+    private AudioSource _playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,7 @@ public class PlayerController : MonoBehaviour
      _playerRB = GetComponent<Rigidbody>();
         _playerAnim = GetComponent<Animator>();
         Physics.gravity *= GravityModifier;
+        _playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,15 +32,18 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && IsOnGround && !gameOver)
             {
+            dirtParticle.Stop();
               _playerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
                IsOnGround = false;
             _playerAnim.SetTrigger("Jump_trig");
+            _playerAudio.PlayOneShot(jumpSound, 1.0f);
             }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             IsOnGround = true;
         }
 
@@ -46,6 +54,8 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
             _playerAnim.SetBool("Death_b", true);
             _playerAnim.SetInteger("DeathType_int)", 1);
+            dirtParticle.Stop();
+            _playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
